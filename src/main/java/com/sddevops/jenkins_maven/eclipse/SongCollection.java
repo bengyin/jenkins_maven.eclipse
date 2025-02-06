011104d3a939c2b06219b4b61da4acb83b560d40
bengyin <bengyin@gmail.com>
2025-02-06 19:08:46 +0800

    Updated SongCollection.java removed commented songs

--- src/main/java/com/sddevops/jenkins_maven/eclipse/SongCollection.java
+++ src/main/java/com/sddevops/jenkins_maven/eclipse/SongCollection.java
@@ -8,10 +8,12 @@ public class SongCollection {
     private int capacity;
 
     public SongCollection() {
-    	/*songs.add(new Song("0001","good 4 u","Olivia Rodrigo",3.59));
-    	songs.add(new Song("0002","Peaches","Justin Bieber",3.18));
-    	songs.add(new Song("0003","MONTERO","Lil Nas X",2.3));
-    	songs.add(new Song("0004","bad guy","Billie Eilish",3.14));*/
+    	/*
+    	 * songs.add(new Song("0001","good 4 u","Olivia Rodrigo",3.59));
+    	 * songs.add(new Song("0002","Peaches","Justin Bieber",3.18));
+    	 * songs.add(new Song("0003","MONTERO","Lil Nas X",2.3));
+    	 * songs.add(new Song("0004","bad guy","Billie Eilish",3.14));
+         */
 
         this.capacity = 20;
     }
